ef4aa4fe5091e4520c93a7b0d9137b6623966b8b
LitzyViridianaBravoAldana <viri30059@example.com>
2023-11-07 18:32:16 -0600

    add

--- APITESCHI/APITESCHI/settings.py
+++ APITESCHI/APITESCHI/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-07)wkvl-k!_#4-hzp$$!bkw^il6hk1bc25uubd193cx0ld5can'
+SECRET_KEY = 'django-insecure-07)wkvl-k!_#4-hzp$$!bkw^il6hk1bc25uubd193cx0ld5can'
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
