25a74fcadea0a36a40c07e397b044aa69204aa41
LitzyViridianaBravoAldana <viri30059@example.com>
2023-11-08 18:23:16 -0600

    modificacion de inicio de sesion en render

--- APITESCHI/APITESCHI/urls.py
+++ APITESCHI/APITESCHI/urls.py
@@ -5,14 +5,14 @@ from api.views import sign_in, sign_up, index_9, team, biblioteca, genero1, gene
 urlpatterns = [
     # Ruta para la página index_9
     #path('index_9.html', index_9.as_view(), name='index_9'),
-    path('', index_9.as_view(), name='index_9'),
+    path('index_9/', index_9.as_view(), name='index_9'),
 
 
     # Ruta para la URL raíz (puede ser una de las páginas anteriores)
     #path('', index_9.as_view(), name='index_9_root'),
 
     # Ruta para la página de inicio de sesión
-    path('sign_in/', sign_in.as_view(), name='sign_in'),
+    path('', sign_in.as_view(), name='sign_in'),
     
     # Ruta para la página de registro
     path('sign_up/', sign_up.as_view(), name='sign_up'),
